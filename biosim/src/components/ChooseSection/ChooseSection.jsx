57829295d071cdf5a39eb95952be0e80725170b5
Rexnice <ernestrex1996@gmail.com>
2024-05-25 12:52:04 +0100

    2505204 commit

--- biosim/src/components/ChooseSection/ChooseSection.jsx
+++ biosim/src/components/ChooseSection/ChooseSection.jsx
@@ -12,8 +12,41 @@ function ChooseSection() {
                 <div className='col-lg-4'>
                     <Card className='d-flex align-items-center border-0 h-100'>
                         <div className='mt-3'>
-
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="red" class="bi bi-journal" viewBox="0 0 16 16">
+                            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
+                            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
+                            </svg>
                         </div>
+                        <Card.Body>
+                            <Card.Title className='text-center text-capitalize mb-3'>
+                                amazing research and publication culture
+                            </Card.Title>
+                            <Card.Text className='text-center'>
+                                At the computational and Bio Simulation Research group,
+                                we carry out ground breaking research work and publish our
+                                results in highly revered journals.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className='col-lg-4'>
+                    <Card className='d-flex align-items-center border-0 h-100'>
+                        <div className='mt-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="red" class="bi bi-journal" viewBox="0 0 16 16">
+                            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
+                            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
+                            </svg>
+                        </div>
+                        <Card.Body>
+                            <Card.Title className='text-center text-capitalize mb-3'>
+                                amazing research and publication culture
+                            </Card.Title>
+                            <Card.Text className='text-center'>
+                                At the computational and Bio Simulation Research group,
+                                we carry out ground breaking research work and publish our
+                                results in highly revered journals.
+                            </Card.Text>
+                        </Card.Body>
                     </Card>
                 </div>
             </div>
